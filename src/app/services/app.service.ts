3962f9be0d6a0a27fa2c9c284e5dbc83787fae45
jmanero <ingmanerojoaquin@gmail.com>
2023-12-11 20:27:00 -0300

    cambios

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 export class AppService {
   constructor(private readonly http: HttpClient) {}
   private baseUrl = 'http://localhost:3000';
+
   getMessage(): Observable<any> {
     return this.http.get<any>(this.baseUrl);
   }
