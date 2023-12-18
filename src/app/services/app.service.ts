0d769b6ced99f4d4ce1dd2c94e7043e7f35bb838
jmanero <ingmanerojoaquin@gmail.com>
2023-12-17 22:35:44 -0300

    cambios en front

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -8,10 +8,10 @@ import { Observable } from 'rxjs';
 export class AppService {
   constructor(private readonly http: HttpClient) {}
   private baseUrl =
-    'http://ec2-18-116-13-180.us-east-2.compute.amazonaws.com:3000';
+    'http://ec2-18-227-52-124.us-east-2.compute.amazonaws.com:3000';
 
   private baseUrlApi =
-    'http://ec2-18-116-13-180.us-east-2.compute.amazonaws.com:3000/files';
+    'http://ec2-18-227-52-124.us-east-2.compute.amazonaws.com:3000/files';
 
   getMessage(): Observable<any> {
     return this.http.get<any>(this.baseUrl);
